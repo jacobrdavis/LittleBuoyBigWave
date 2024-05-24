45695fd5c6ad6eefb9d7aecb4a9c214d7b9b6046
jacobrdavis <jacobdavis4085@gmail.com>
2024-05-24 14:01:54 -0700

    move accessors to dataarray

--- littlebuoybigwaves/models/xarray_accessor.py
+++ littlebuoybigwaves/models/xarray_accessor.py
@@ -50,7 +50,7 @@ class ModelDatasetAccessor:
         scalar frequency spectra and return it as a DataArray.
         """
         fq_dir_spectrum = self._obj[self.vars.frequency_direction_energy_density]
-        fq_spectrum = fq_dir_spectrum.integrate(coord=self.vars.direction)
+        fq_spectrum = fq_dir_spectrum.model.fq_dir_spectrum_to_fq_spectrum()
         return fq_spectrum
 
     # def colocate_with_path_ds(...):  TODO: see example from pywsra
@@ -58,14 +58,8 @@ class ModelDatasetAccessor:
 
     def mean_square_slope(self) -> xr.DataArray:
         """ Calculate mean square slope and return it as a DataArray. """
-        mean_square_slope = xr.apply_ufunc(
-            waves.mean_square_slope,
-            self._obj[self.vars.frequency_energy_density],
-            self._obj[self.vars.frequency],
-            input_core_dims=[[self.vars.frequency], [self.vars.frequency]],
-            output_core_dims=[[]],
-            vectorize=True,
-        )
+        fq_spectrum = self._obj[self.vars.frequency_energy_density]
+        mean_square_slope = fq_spectrum.model.mean_square_slope()
         return mean_square_slope
 
 
@@ -75,6 +69,31 @@ class ModelDataArrayAccessor:
     """ Extend xarray DataArray objects with model-specific functionality. """
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
-        self._center = None
+        self._vars = var_namespace
 
-        #TODO: spectal var, etc.
+    @property
+    def vars(self):  #TODO: is this valid here?
+        """ Return a SimpleNamespace with this DataArray's variable names. """
+        return self._vars
+
+    def fq_dir_spectrum_to_fq_spectrum(self) -> xr.DataArray:
+        """
+        Integrate frequency-direction spectra over direction to obtain
+        scalar frequency spectra and return it as a DataArray.
+        """
+        fq_spectrum = self._obj.integrate(coord=self.vars.direction)
+        return fq_spectrum
+
+    def mean_square_slope(self) -> xr.DataArray:
+        """ Calculate mean square slope and return it as a DataArray. """
+        mean_square_slope = xr.apply_ufunc(
+            waves.mean_square_slope,
+            self._obj,
+            self._obj[self.vars.frequency],
+            input_core_dims=[[self.vars.frequency], [self.vars.frequency]],
+            output_core_dims=[[]],
+            vectorize=True,
+        )
+        return mean_square_slope
+
+    #TODO: spectal var, etc.
